093bc8484585e37b6410ae509a73170a07ab374d
Moneera Banjar <45167008+mn-banjar@users.noreply.github.com>
2024-08-13 19:23:36 +0300

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import numpy as np 
+import pickle
+from flask import Flask, render_template, request
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app=Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+trip = pd.read_csv('tripadvisor_hotel_reviews.csv')
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(trip['Review'])
+
+
+
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html', **locals())
+
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    comment = request.form['comment']
+
+    # Tokenization
+    comment = tokenizer.texts_to_sequences(comment)
+
+    # padding
+    comment = pad_sequences(comment,maxlen=100,padding='post')
+
+    # prediction
+    review_predict = (model.predict(comment)>0.5).astype('int')
+
+    if review_predict[0] == 1:
+        result = "It's a negative review"
+    else:
+        result = "It's a positive review"
+
+    return render_template('index.html', **locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
